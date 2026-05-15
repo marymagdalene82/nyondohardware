7e090477f7d953c1cf5a7cb35b49cb8e21f0c184
Mary Magdalene <mary.achero@students.mak.ac.ug>
2026-05-15 16:34:15 +0300

    feat: register deposit models in admin panel

--- nyondo_stock/nyondo/admin.py
+++ nyondo_stock/nyondo/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category, Product, Supplier, StockEntry, Sale, SaleItem
+from .models import Category, Product, Supplier, StockEntry, Sale, SaleItem,DepositCustomer, DepositTransaction, DepositPickup, DepositPickupItem
+
+admin.site.register(DepositCustomer)
+admin.site.register(DepositTransaction)
+admin.site.register(DepositPickup)
+admin.site.register(DepositPickupItem)
 
 admin.site.register(Category)
 admin.site.register(Product)
